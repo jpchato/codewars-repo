0d57e645667670ab196711d51494529c0e5f6dd3
jpchato <jpchato@gmail.com>
2021-02-18 06:56:01 -0800

    find int that appears odd times, codewars solution

---
+++ codewars/6th_kyu/find_odd_int.py
@@ -0,0 +1,30 @@
+'''
+https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+'''
+def find_it(seq):
+    odd_dict = {
+
+    }
+    
+    for number in seq:
+        # if number % 2 != 0 and number not in odd_dict:
+        if number not in odd_dict:
+            
+            odd_dict[number] = 1
+        # elif number % 2 != 0 and number in odd_dict:
+        elif number in odd_dict:
+            odd_dict[number] += 1
+    print(odd_dict)
+    
+    for key, value in odd_dict.items():
+        if value % 2 != 0:
+            
+            return key
+    
+
+if __name__== '__main__':
+    # find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+    find_it([10])
