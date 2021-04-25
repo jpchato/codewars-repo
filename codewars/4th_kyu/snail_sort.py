57e844ac503be33a245a526374e3be7a28e07847
jpchato <jpchato@gmail.com>
2021-04-24 17:36:15 -0700

    snail sort

--- codewars/4th_kyu/snail_sort.py
+++ codewars/4th_kyu/snail_sort.py
@@ -23,38 +23,34 @@ python slicing: [start:end:step]
 '''
 
 def snail(snail_map):
-    new_array= []
-    for item in snail_map[0]:
-        new_array.append(item)
-    for array in snail_map[1:-1:]:
-        new_array.append(array[-1])
-    for item in snail_map[-1][::-1]:
-        new_array.append(item)
-    for array in snail_map[-2:0:-1]:
-        # print(array)
-        new_array.append(array[0])
-    print(new_array)
+    results = []
+    while len(snail_map) > 0:
+        # go right
+        results += snail_map[0]
+        del snail_map[0]
+
+        if len(snail_map) > 0:
+            # go down
+            for i in snail_map:
+                results += [i[-1]]
+                del i[-1]
+            # go left
+            if snail_map[-1]:
+                # print('snail_map[-1][::-1] = ', snail_map[-1][::-1])
+                # appending the last array in reversed order
+                results += snail_map[-1][::-1]
+                del snail_map[-1]
+            # go top
+            for i in reversed(snail_map):
+                results += [i[0]]
+                del i[0]
+    return results
+
+
+   
 
 if __name__ == '__main__':
     snail([[1,2,3],
          [4,5,6],
          [7,8,9]])
         #  expected = [1,2,3,6,9,8,7,4,5]
-
-# def snail(snail_map):
-#     new_array = []
-#     for item in snail_map[0]:
-#        new_array.append(item)
-#     new_array.append(snail_map[1][-1])
-#     for item in snail_map[-1][::-1]:
-#         new_array.append(item)
-#     for item in snail_map[1][0:2:]:
-#         new_array.append(item)
-#     print(new_array)
-#     return new_array
-
-# algorithm
-# the first array is added to the new array in sequential order
-# the last element of the remaining arrays is added to the new array until we reach the last array
-# the last array is then added in reverse order to the new array
-# then the first element of the remaining arrays is added (in reverse order)
