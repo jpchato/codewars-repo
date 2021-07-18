8ac2132942d257a5cd4772e54bb7d532edf80e2e
jpchato <jpchato@gmail.com>
2021-07-18 16:08:03 -0700

    boolean to word 8th kyu

---
+++ codewars/8th_kyu/bool_to_word.py
@@ -0,0 +1,13 @@
+'''
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+'''
+
+def bool_to_word(boolean):
+    if boolean == True:
+        return 'Yes'
+    else:
+        return 'No'
+    
+
+bool_to_word(True)#, 'Yes'
+bool_to_word(False)#, 'No'
