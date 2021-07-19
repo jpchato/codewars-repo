96687dd698338f9b85f5464c15eb5a06f2df164f
jpchato <jpchato@gmail.com>
2021-07-18 18:41:44 -0700

    replace with alphabet position, 6th kyu

---
+++ codewars/6th_kyu/replace_with_alphabet_position.py
@@ -0,0 +1,26 @@
+'''
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabet_position("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+'''
+
+def alphabet_position(text):
+    alphabet_dict = {'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4, 'g': 7, 'f': 6, 'i': 9, 'h': 8, 'k': 11, 'j': 10, 'm': 13, 'l': 12, 'o': 15, 'n': 14, 'q': 17, 'p': 16, 's': 19, 'r': 18, 'u': 21, 't': 20, 'w': 23, 'v': 22, 'y': 25, 'x': 24, 'z': 26}
+    return_string = ''
+    for item in text.lower():
+        if item in alphabet_dict:
+            print(item)
+            return_string += str(alphabet_dict[item]) + ' '
+    print(return_string[:-1])
+    return return_string[:-1]
+
+# alphabet_position("The sunset sets at twelve o' clock.")#, "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
+alphabet_position("The narwhal bacons at midnight.")#, "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20")
