50f45246b3e7a83a3f587a1479d5591cd4eb229a
jpchato <jpchato@gmail.com>
2021-07-31 14:34:46 -0700

    reversed strings, 8th kyu

---
+++ codewars/8th_kyu/reversed_strings.py
@@ -0,0 +1,12 @@
+def solution(string):
+    '''
+    Complete the solution so that it reverses the string passed into it.
+
+    'world'  =>  'dlrow'
+    '''
+
+def solution(str):
+    # print(str[-1::-1])
+    return str[-1::-1]
+
+solution('world')#, 'dlrow'
