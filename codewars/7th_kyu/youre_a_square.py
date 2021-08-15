741a9d09b471b82448d1bd6718e09dbc3777d4d6
jpchato <jpchato@gmail.com>
2021-08-15 10:04:55 -0700

    ssh test

--- codewars/7th_kyu/youre_a_square.py
+++ codewars/7th_kyu/youre_a_square.py
@@ -38,4 +38,4 @@ def is_square(n):
 # is_square(25)#, True, "25 is a square number (5 * 5)"
 # is_square( 0)#, True, "0 is a square number (0 * 0)")
 # is_square( 3)#, False, "3 is not a square number"
-is_square( 4)#, True, "4 is a square number (2 * 2)"  #asdf
+is_square( 4)#, True, "4 is a square number (2 * 2)"  #asdfsadfasdf
